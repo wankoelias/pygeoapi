7c22db59346db1386daedaeb1af247316f5b9835
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-09-10 15:48:43 +0200

    Set up url dispatching
    /oapi goes to pygeoapi
    Everything else to ogc-edc

--- dispatch_app.py
+++ dispatch_app.py
@@ -1,9 +1,9 @@
 # TODO: license
 
-
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from pygeoapi.flask_app import APP as pygeoapi_app
 from edc_ogc.app import app as edc_ogc_app
 
-app = DispatcherMiddleware(pygeoapi_app, {'/ogc-edc': edc_ogc_app})
+app = DispatcherMiddleware(edc_ogc_app, {'/oapi': pygeoapi_app})
+
