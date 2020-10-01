d51cdc2bde21e5bc144fa1472788368a88434e61
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-10-01 12:01:34 +0200

    Manage job metadata by manager, not processor

--- pygeoapi/process/notebook.py
+++ pygeoapi/process/notebook.py
@@ -34,7 +34,7 @@ from typing import Dict
 
 from kubernetes import client as k8s_client, config as k8s_config
 
-from pygeoapi.process.manager.k8s import KubernetesProcessor
+from pygeoapi.process.manager.kubernetes import KubernetesProcessor
 
 LOGGER = logging.getLogger(__name__)
 
@@ -102,87 +102,69 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
     def __init__(self, processor_def):
         super().__init__(processor_def, PROCESS_METADATA)
 
-    def create_job(self, data: Dict) -> k8s_client.V1Job:
-        return create_notebook_job(
-            notebook_path=data["notebook"], parameters=data["parameters"]
+    def create_job_pod_spec(self, data: Dict) -> k8s_client.V1PodSpec:
+        notebook_path = data["notebook"]
+        parameters = data["parameters"]
+        job_name = "job-notebook"
+
+        home = Path("/home/jovyan")
+
+        # TODO: configurable
+        home_pvc = "edc-dev-jupyter-user"
+
+        # TODO: from env
+        home_subpath = "users/0c0a0ef4-2d8d0d-2d4e70-2db002-2d033c7cbb41fd"
+
+        # TODO: fetch these values from profile (will require profile selection in future)
+        image = "eurodatacube/jupyter-user:0.20.1"
+        kernel = "edc"
+        cpu_limit = "2"
+        memory_limit = "4G"
+
+        # TODO: include date(time)
+        output_notebook = re.sub(".ipynb$", "", notebook_path) + "_result.ipynb"
+
+        container = k8s_client.V1Container(
+            name=job_name,
+            image=image,
+            command=[
+                "bash",
+                "-c",
+                f"/opt/conda/envs/*/bin/papermill "
+                f'"{notebook_path}" '
+                f'"{output_notebook}" '
+                f'-b "{parameters}" '
+                f"-k {kernel}",
+            ],
+            working_dir=str(home),
+            volume_mounts=[
+                k8s_client.V1VolumeMount(
+                    mount_path=str(home), name="home", sub_path=home_subpath
+                ),
+            ],
+            resources=k8s_client.V1ResourceRequirements(
+                limits={
+                    "cpu": cpu_limit,
+                    "memory": memory_limit,
+                }
+            ),
+            env=[],
+        )
+
+        volumes = [
+            k8s_client.V1Volume(
+                persistent_volume_claim=k8s_client.V1PersistentVolumeClaimVolumeSource(
+                    claim_name=home_pvc,
+                ),
+                name="home",
+            ),
+        ]
+
+        return k8s_client.V1PodSpec(
+            restart_policy="Never",
+            containers=[container],
+            volumes=volumes,
         )
 
     def __repr__(self):
         return "<PapermillNotebookKubernetesProcessor> {}".format(self.name)
-
-
-def create_notebook_job(
-    notebook_path: str,
-    parameters: str,
-) -> k8s_client.V1Job:
-    job_name = "job-notebook"
-
-    home = Path("/home/jovyan")
-
-    # TODO: configurable
-    home_pvc = "edc-dev-jupyter-user"
-
-    # TODO: from env
-    home_subpath = "users/0c0a0ef4-2d8d0d-2d4e70-2db002-2d033c7cbb41fd"
-
-    # TODO: fetch these values from profile (will require profile selection in future)
-    image = "eurodatacube/jupyter-user:0.20.1"
-    kernel = "edc"
-    cpu_limit = "2"
-    memory_limit = "4G"
-
-    # TODO: include date(time)
-    output_notebook = re.sub(".ipynb$", "", notebook_path) + "_result.ipynb"
-
-    container = k8s_client.V1Container(
-        name=job_name,
-        image=image,
-        command=[
-            "bash",
-            "-c",
-            f"/opt/conda/envs/*/bin/papermill "
-            f'"{notebook_path}" '
-            f'"{output_notebook}" '
-            f'-b "{parameters}" '
-            f"-k {kernel}",
-        ],
-        working_dir=str(home),
-        volume_mounts=[
-            k8s_client.V1VolumeMount(
-                mount_path=str(home), name="home", sub_path=home_subpath
-            ),
-        ],
-        resources=k8s_client.V1ResourceRequirements(
-            limits={
-                "cpu": cpu_limit,
-                "memory": memory_limit,
-            }
-        ),
-        env=[],
-    )
-
-    volumes = [
-        k8s_client.V1Volume(
-            persistent_volume_claim=k8s_client.V1PersistentVolumeClaimVolumeSource(
-                claim_name=home_pvc,
-            ),
-            name="home",
-        ),
-    ]
-
-    return k8s_client.V1Job(
-        api_version="batch/v1",
-        kind="Job",
-        metadata=k8s_client.V1ObjectMeta(name=job_name),
-        spec=k8s_client.V1JobSpec(
-            template=k8s_client.V1PodTemplateSpec(
-                spec=k8s_client.V1PodSpec(
-                    restart_policy="Never",
-                    containers=[container],
-                    volumes=volumes,
-                ),
-            ),
-            backoff_limit=0,
-            ttl_seconds_after_finished=60 * 60 * 24 * 7,
-        ),
-    )
