9ff405d2dde41a72fcfc5530b5bbcdb7a401ad50
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-10-05 13:18:18 +0200

    Use basic profile settings from customer-operator

--- pygeoapi/process/notebook.py
+++ pygeoapi/process/notebook.py
@@ -27,18 +27,30 @@
 #
 # =================================================================
 
+from __future__ import annotations
+
+from dataclasses import dataclass, fields
 from datetime import datetime
 import logging
 from pathlib import Path
+import os
 import re
 from typing import Dict, Tuple
 import urllib.parse
 
-from kubernetes import client as k8s_client, config as k8s_config
+from kubernetes import client as k8s_client
+import requests
+from requests.models import HTTPBasicAuth
 
 from pygeoapi.process.manager.kubernetes import KubernetesProcessor
 
 LOGGER = logging.getLogger(__name__)
+
+# move to some config file?
+customer_operator_auth = HTTPBasicAuth(
+    username=os.environ["CUSTOMER_OPERATOR_USER"],
+    password=os.environ["CUSTOMER_OPERATOR_PASSWORD"],
+)
 
 #: Process metadata and description
 PROCESS_METADATA = {
@@ -108,6 +120,7 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
         self,
         data: Dict,
         user_uuid: str,
+        user_email: str,
     ) -> Tuple[k8s_client.V1PodSpec, Dict]:
         notebook_path = data["notebook"]
         parameters = data["parameters"]
@@ -121,11 +134,11 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
         # TODO: from env
         home_subpath = f"users/{user_uuid}".replace("-", "-2d")
 
-        # TODO: fetch these values from profile (will require profile selection in future)
-        image = "eurodatacube/jupyter-user:0.20.1"
-        kernel = "edc"
-        cpu_limit = "2"
-        memory_limit = "4G"
+        profile = retrieve_profile(user_uuid=user_uuid, user_email=user_email)
+
+        # be a bit smart to select kernel (this should do for now,
+        is_gpu = profile.image.split(":")[0].endswith("-g")
+        kernel = "edc-gpu" if is_gpu else "edc"
 
         filename_without_postfix = re.sub(".ipynb$", "", notebook_path)
         now_formatted = datetime.now().strftime("%y%m%d-%H%M")
@@ -133,7 +146,7 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
 
         container = k8s_client.V1Container(
             name=job_name,
-            image=image,
+            image=profile.image,
             command=[
                 "bash",
                 "-c",
@@ -150,10 +163,11 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
                 ),
             ],
             resources=k8s_client.V1ResourceRequirements(
-                limits={
-                    "cpu": cpu_limit,
-                    "memory": memory_limit,
-                }
+                limits={"cpu": profile.cpu_limit, "memory": profile.mem_limit},
+                requests={
+                    "cpu": profile.cpu_guarantee,
+                    "memory": profile.mem_guarantee,
+                },
             ),
             env=[],
         )
@@ -191,3 +205,35 @@ class PapermillNotebookKubernetesProcessor(KubernetesProcessor):
 
     def __repr__(self):
         return "<PapermillNotebookKubernetesProcessor> {}".format(self.name)
+
+
+@dataclass(frozen=True)
+class EDCProfile:
+    image: str
+    mem_limit: str
+    mem_guarantee: str
+    cpu_limit: str
+    cpu_guarantee: str
+
+    @classmethod
+    def from_superset(cls, d: Dict) -> EDCProfile:
+        field_names = [field.name for field in fields(cls)]
+        return cls(**{k: v for k, v in d.items() if k in field_names})
+
+
+def retrieve_profile(user_uuid: str, user_email: str) -> EDCProfile:
+    LOGGER.debug(f"Retrieving profiles for {user_uuid} {user_email}")
+    profiles = requests.get(
+        f"https://customer-operator.dev.hub.eox.at/tenants/{user_uuid}/profiles?"
+        f"email={urllib.parse.quote(user_email)}",
+        auth=customer_operator_auth,
+    ).json()
+
+    LOGGER.debug("Got profiles %s", profiles)
+
+    try:
+        profile = profiles[0]
+    except KeyError as e:
+        raise RuntimeError("No active user profile found") from e
+
+    return EDCProfile.from_superset(profile)
